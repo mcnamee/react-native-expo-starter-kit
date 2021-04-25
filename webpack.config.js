1b13fb4940ff72702566c416cd05c1dfafe2e95d
Lakhveer Bawa <bawa93lakhveer@gmail.com>
2021-04-25 17:10:27 +0530

    Fix: react-native-keyboard-aware-scroll-view issue where it gives error and does not compile
    https://forums.expo.io/t/expo-start-web-failed-to-compile-after-import-native-base/40826/9

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+    const config = await createExpoWebpackConfigAsync({
+        ...env,
+        babel: {
+            dangerouslyAddModulePathsToTranspile: ['@codler/react-native-keyboard-aware-scroll-view']
+        }
+    }, argv);
+    return config;
+};
